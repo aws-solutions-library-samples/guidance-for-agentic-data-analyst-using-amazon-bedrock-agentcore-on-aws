7a2443a9aca67b752fce05da875295a38180eb98
Emilio Monti <monti@amazon.com>
2026-01-09 17:02:20 +0000

    Add eighth test.

---
+++ data/tests/8/script.py
@@ -0,0 +1,11 @@
+df = query_handler.query_ons_dataset('mid-year-pop-est', {
+    'geography': 'Blaenau Gwent',
+    'age': '30',
+    'sex': 'All'
+})
+
+df['time_int'] = df['time'].astype(int)
+df_sorted = df.sort_values('time_int', ascending=False)
+latest = df_sorted.iloc[0]
+
+print(f"Latest data for 30 year-olds in Blaenau Gwent ({latest['time']}): {latest['observation']}")
