3f1588b082d59f523f0b418d0da48f3aa10f6d55
Emilio Monti <monti@amazon.com>
2026-01-09 17:38:23 +0000

    Add eleventh and last test.

---
+++ data/tests/11/script.py
@@ -0,0 +1,12 @@
+df = query_handler.query_ons_dataset('weekly-deaths-age-sex', {
+    'geography': 'London',
+    'agegroups': '70-74',
+    'registrationoroccurrence': 'Occurrences',
+    'sex': 'All'
+})
+
+df['week_num'] = df['week'].str.extract(r'Week (\d+)').astype(int)
+latest = df.sort_values(['time', 'week_num']).iloc[-1]
+
+print("The ONS does not publish weekly death statistics broken down by single year of age (72 years).")
+print(f"However, the closest available data shows that in Week {latest['week_num']}, {latest['time']}, there were {latest['observation']} deaths of people aged 70-74 in London.")
