8de1ee24a0ef8c8ffc5058b3741a1718bb89c894
Emilio Monti <monti@amazon.com>
2026-01-09 17:10:00 +0000

    Add nineth test.

---
+++ data/tests/9/script.py
@@ -0,0 +1,11 @@
+df = query_handler.query_ons_dataset('mid-year-pop-est', {
+    'geography': 'ENGLAND AND WALES',
+    'age': '90+',
+    'sex': 'All'
+})
+
+df['time'] = df['time'].astype(int)
+latest = df[df['time'] == df['time'].max()]
+latest = latest.iloc[0]
+
+print(f"As of {latest['time']}, the population aged 90 or over in England and Wales is: {latest['observation']}")
