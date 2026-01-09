ee7522536c2e32da5e80d3fc53c22d03056c80ba
Emilio Monti <monti@amazon.com>
2026-01-09 12:31:52 +0000

    rename test directory to data.

--- agent/aws_data_analyst/evaluation/__init__.py
+++ agent/aws_data_analyst/evaluation/__init__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 
 REPOSITORY_DIR = Path(__file__).parent.parent.parent.parent
-TEST_DATA_DIR = REPOSITORY_DIR / 'test'
+TEST_DATA_DIR = REPOSITORY_DIR / 'data'
 TESTS_PATH = TEST_DATA_DIR / 'queries.json'
